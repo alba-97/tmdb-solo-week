87d03d148188ebb4ff02bef6615c2f28ebc3cff5
alba-97 <alvarezbasilio97@gmail.com>
2026-03-24 02:26:46 -0300

    remove unused files

--- server/config/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const config = {
-  api: process.env.TMDB_API_KEY,
-  secret: process.env.JWT_SECRET,
-  db: process.env.DB_NAME,
-};
-
-module.exports = config;
--- server/models/index.js
+++
@@ -1,7 +0,0 @@
-const Users = require("./Users");
-const Items = require("./Items");
-
-Items.belongsToMany(Users, { through: "favorites" });
-Users.belongsToMany(Items, { through: "favorites" });
-
-module.exports = { Users, Items };
